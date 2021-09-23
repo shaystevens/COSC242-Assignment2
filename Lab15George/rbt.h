9a7c36cbd572d5af467613895117e6af8713e532
gchurton <chuge803@student.otago.ac.nz>
2021-09-23 21:22:59 +1200

    Upload New File

---
+++ Lab15George/rbt.h
@@ -0,0 +1,13 @@
+#ifndef RBT_H_
+#define RBT_H_
+
+typedef struct rbt_node *rbt;
+
+extern rbt rbt_delete(rbt r, char *str);
+extern rbt rbt_free(rbt r);
+extern void rbt_inorder(rbt r, void f(char *str));
+extern rbt rbt_insert(rbt r, char *str);
+extern rbt rbt_new();
+extern void rbt_preorder(rbt r, void f(char *str));
+extern int rbt_search(rbt r, char *str);
+#endif
