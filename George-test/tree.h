0491ba5352f6a4998507d1e225cee0fa84f519fb
gchurton <chuge803@student.otago.ac.nz>
2021-09-28 01:24:10 +1300

    added tree_output_dot function

--- George-test/tree.h
+++ George-test/tree.h
@@ -13,6 +13,6 @@ extern void tree_preorder(tree t, void f(int freq, char *str));
 extern int tree_search(tree t, char *str);
 extern int tree_depth(tree t);
 extern void tree_output_dot(tree t, FILE *out);
-extern void tree_output_dot_aux(tree t, FILE *out);
+
 
 #endif
