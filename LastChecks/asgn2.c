202bf79f0689d499c80d4aea6da3d4b76a53eec4
mwong <wonma004@student.otago.ac.nz>
2021-10-08 02:44:12 +1300

    Created LastChecks folder, removed redundancies from asgn2.c, added/edited comments

---
+++ LastChecks/asgn2.c
@@ -0,0 +1,205 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "mylib.h"
+#include "tree.h"
+
+/**
+ * Prints out the keys/words and how many times they were input into the tree.
+ *
+ * @param freq - int, how many times a key/word were put into the tree.
+ * @param word - char *, the words inputted into the tree.
+ */
+static void print_info(int freq, char *word) {
+    printf("%-4d %s\n", freq, word);
+}
+
+/**
+ * Prints out a helpful messsage on what the program does, showing what
+ * arguments you can enter in the command line and what they do.
+ *
+ * @param x - int, to determine if help message called due to -h option given
+ * or because invalid option given.
+ */
+static void help_message(int x){
+    if(x == 0){
+        fprintf(stdout, "Usage: ./asgn2 [OPTION]... <STDIN>\n\n");
+        fprintf(stdout, "Perform various operations using a binary tree.  "
+                "By default, words\n");
+        fprintf(stdout, "are read from stdin and added to the tree, before"
+                " being printed out\n");
+        fprintf(stdout, "alongside their frequencies to stdout.\n\n");
+        fprintf(stdout, " -c FILENAME  Check spelling of words in FILENAME"
+                " using words\n");
+        fprintf(stdout, "              read from stdin as the dictionary."
+                "  Print timing\n");
+        fprintf(stdout, "              info & unknown words to stderr "
+                "(ignore -d & -o)\n");
+        fprintf(stdout, " -d           Only print the tree depth"
+                " (ignore -o)\n");
+        fprintf(stdout, " -f FILENAME  Write DOT output to FILENAME (if -o "
+                "given)\n");
+        fprintf(stdout, " -o           Output the tree in DOT form to file"
+                " 'tree-view.dot' \n");
+        fprintf(stdout, " -r           Make the tree an RBT (the default"
+                " is a BST)\n\n");
+        fprintf(stdout, " -h           Print this message\n\n");
+    }else{
+        fprintf(stderr, "Usage: ./asgn2 [OPTION]... <STDIN>\n\n");
+        fprintf(stderr, "Perform various operations using a binary tree.  "
+                "By default, words\n");
+        fprintf(stderr, "are read from stdin and added to the tree, before"
+                " being printed out\n");
+        fprintf(stderr, "alongside their frequencies to stdout.\n\n");
+        fprintf(stderr, " -c FILENAME  Check spelling of words in FILENAME"
+                " using words\n");
+        fprintf(stderr, "              read from stdin as the dictionary."
+                "  Print timing\n");
+        fprintf(stderr, "              info & unknown words to stderr "
+                "(ignore -d & -o)\n");
+        fprintf(stderr, " -d           Only print the tree depth"
+                " (ignore -o)\n");
+        fprintf(stderr, " -f FILENAME  Write DOT output to FILENAME (if -o "
+                "given)\n");
+        fprintf(stderr, " -o           Output the tree in DOT form to file"
+                " 'tree-view.dot' \n");
+        fprintf(stderr, " -r           Make the tree an RBT (the default"
+                " is a BST)\n\n");
+        fprintf(stderr, " -h           Print this message\n\n");
+    } 
+}
+
+/**
+ * Main part of the program, which reads inputs from stdin and changes the
+ * program's output. Creates a tree using tree type (default is a BST).
+ *
+ * @param argc - int, number of arguments.
+ * @param argv - char **, the arguments.
+ *
+ * @return int, EXIT_SUCCESS if program is successful, or EXIT_FAILURE.
+ */
+int main(int argc, char **argv){
+
+    /* Initialising and declaring variables */
+    tree t;
+    int unknown_words = 0;
+    clock_t fillStart, fillEnd, searchStart, searchEnd;
+    double fillTime, searchTime;
+    char word[256];
+    const char *optstring = "dorhf:c:";
+
+    FILE *filename = NULL;
+    FILE *dotfile;
+    
+    /* Used to store which options were given on the command line */
+    int option = 0;
+    int print_depth_flag = 0;
+    int check_file = 0;
+    int output = 0;
+    int this_tree_depth;
+    char *changed_dotfile_name = "";
+    
+    static type_t tree_type = BST;
+
+    /* Use getopt to get arguments from the command line */
+    if (argc > 0){
+        while ((option = getopt(argc, argv, optstring)) != EOF) {
+            switch (option) {
+                case 'c':
+                    if(NULL == (filename = fopen(optarg, "r"))){
+                       return EXIT_FAILURE;
+                    }
+                    check_file = 1;
+                    break;
+
+                case 'd': 
+                    print_depth_flag = 1;
+                    break;
+
+                case 'f':                   
+                    /* dot_out_filename = 1;*/
+                    changed_dotfile_name = optarg;
+                    break;
+
+                case 'o':
+                    output = 1;
+                    break;	
+
+                case 'r':
+                    tree_type = RBT;
+                    break;
+
+                case 'h':
+                    help_message(0);
+                    return EXIT_SUCCESS;
+                    break;
+
+                default:
+                    help_message(1);
+                    return EXIT_SUCCESS;
+                    break;
+            }
+        }
+    }
+
+    t = tree_new(tree_type);
+
+    fillStart = clock();
+    /*fill work*/
+    while (getword(word, sizeof word, stdin) != EOF){
+        t = tree_insert(t, word);
+    }
+    fillEnd = clock();
+
+    /* -c argument */
+    if(check_file == 1){
+        searchStart = clock();       
+        /*search work*/
+        while (getword(word, sizeof word, filename) != EOF){
+            if(tree_search(t, word) != 1){
+                unknown_words++;
+                fprintf(stdout, "%s\n", word);
+            }
+        }
+        searchEnd = clock();
+        /* Print results int a table to stderr */
+        fillTime = ((double) (fillEnd -  fillStart))/CLOCKS_PER_SEC;
+        searchTime = ((double) (searchEnd - searchStart))/CLOCKS_PER_SEC;
+        fprintf(stderr, "Fill time     : %f\n", fillTime);
+        fprintf(stderr, "Search time   : %f\n", searchTime);
+        fprintf(stderr, "Unknown words = %d\n", unknown_words);
+    }
+
+    /* -d argument */
+    else if(print_depth_flag == 1){
+        this_tree_depth = tree_depth(t);
+        fprintf(stdout, "%d\n", this_tree_depth);
+    } 
+       
+    /* -o argument */
+    else if(output == 1){
+        if (strcmp(changed_dotfile_name, "") != 0){
+            /* -f argument */
+            dotfile = fopen(changed_dotfile_name, "w");
+            fprintf(stdout, "Creating dot file '%s'\n", changed_dotfile_name);
+        } else {
+            /* Without -f argument */
+            dotfile = fopen("tree-view.dot", "w");
+            fprintf(stdout, "Creating dot file 'tree-view.dot'\n");            
+        }
+        tree_output_dot(t, dotfile);
+        fclose(dotfile);
+    }
+
+    /* Normal printing of tree with frequencies */
+    else {
+        tree_preorder(t, print_info);
+    }
+
+    /* Deallocation of assigned memory */
+    tree_free(t);
+    return EXIT_SUCCESS;
+}
---
+++ LastChecks/tree.c
@@ -0,0 +1,343 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mylib.h"
+#include "tree.h"
+
+#define IS_BLACK(x) ((NULL == (x)) || (BLACK == (x)->colour))
+#define IS_RED(x) ((NULL != (x)) && (RED == (x)->colour))
+
+/* THINGS TO DO IN THE TREE.C FILE 
+   - TRY THE OUTPUT DOT FUNCTIONS
+   - root node black done, though need to test with output dot function
+ */
+   
+
+typedef enum { RED, BLACK } tree_colour;
+static type_t tree_type;
+static int no_insertion_calls = 0;
+static tree root_node;
+
+/* Tree structure */
+struct tree_node {
+    char *key;
+    tree_colour colour;
+    tree left;
+    tree right;
+    int frequency;
+};
+
+/**
+ * Traverses the tree writing a DOT description about connections, and
+ * possibly colours, to the given output stream.
+ *
+ * @param t the tree to output a DOT description of.
+ * @param out the stream to write the DOT output to.
+ */
+static void tree_output_dot_aux(tree t, FILE *out) {
+    if (t == NULL) {
+        return;
+    }
+    if(t->key != NULL) {
+        fprintf(out, "\"%s\"[label=\"{<f0>%s:%d|{<f1>|<f2>}}\"color=%s];\n",
+                t->key, t->key, t->frequency,
+                (RBT == tree_type && RED == t->colour) ? "red":"black");
+    }
+    if(t->left != NULL) {
+        tree_output_dot_aux(t->left, out);
+        fprintf(out, "\"%s\":f1 -> \"%s\":f0;\n", t->key, t->left->key);
+    }
+    if(t->right != NULL) {
+        tree_output_dot_aux(t->right, out);
+        fprintf(out, "\"%s\":f2 -> \"%s\":f0;\n", t->key, t->right->key);
+    }
+}
+
+/**
+ * Output a DOT description of this tree to the given output stream.
+ * DOT is a plain text graph description language (see www.graphviz.org).
+ * You can create a viewable graph with the command
+ *
+ *    dot -Tpdf < dotfile > dotfile.pdf
+ *
+ * where 'dotfile' is a file that has been written by tree_output_dot()
+ *
+ * You can also use png, ps, jpg, svg... instead of pdf
+ *
+ * @param t the tree to output the DOT description of.
+ * @param out the stream to write the DOT description to.
+ */
+void tree_output_dot(tree t, FILE *out) {
+    fprintf(out, "digraph tree {\nnode [shape = Mrecord, penwidth = 2];\n");
+    tree_output_dot_aux(t, out);
+    fprintf(out, "}\n");
+}
+
+/**
+ * Creates a new tree using and uses the parameter to set the
+ * type of tree it is.
+ *
+ * @param tree_version - type_t sets the tree type.
+ *
+ * @return returns NULL.
+ */
+tree tree_new(type_t tree_version){
+    tree_type = tree_version;
+    return NULL;
+}
+
+/**
+ * Searches the tree for str returns 1 if found or 0 if not.
+ *
+ * @param t - type tree, the tree.
+ * @param str - type char*, the string that is wanted to be found.
+ *
+ * @return int - 1 if string is in tree, 0 if it is not.
+ */
+int tree_search(tree t, char *str){
+    if (t == NULL){
+        return 0;
+    } else {
+        if (strcmp(t->key, str) == 0){
+            return 1;
+        } else {
+            if (strcmp(t->key, str) > 0){
+                return tree_search(t->left, str);
+            } else {
+                return tree_search(t->right, str);
+            }
+        }
+    }
+}
+
+/**
+ * Frees the allocated memory of the tree.
+ *
+ * @param t - type tree, the tree.
+ *
+ * @return returns NULL.
+ */
+tree tree_free(tree t){
+    if (t == NULL){
+        return NULL;
+    }
+    tree_free(t->left);
+    tree_free(t->right);
+    free(t->key);
+    free(t);
+
+    return NULL;
+}
+
+/*void tree_inorder(tree t, void f(char *str)){
+    if (t == NULL){
+        return;
+    } else {
+        tree_inorder(t->left, f);
+        f(t->key);
+        tree_inorder(t->right, f);
+        return;
+    }
+    }*/
+
+/**
+ * Rotates the tree by shifting the the root down to the left.
+ *
+ * @param t - type tree, the tree.
+ *
+ * @return returns the tree.
+ */
+static tree left_rotate(tree t){
+    tree temp;
+
+    temp = t;
+    t = t->right;
+    if (temp == root_node){
+        root_node = t;
+    }
+    temp->right = t->left;
+    t->left = temp;
+
+    return t;
+    
+}
+
+/**
+ * Rotates the tree by shifting the root down the side of the tree.
+ *
+ * @param t - type tree, the tree.
+ *
+ * @return returns the tree.
+ */
+static tree right_rotate(tree t){
+    tree temp;
+
+    temp = t;
+    t = t->left;
+    if (temp == root_node){
+        root_node = t;
+    }
+    temp->left = t->right;
+    t->right = temp;
+
+    return t;
+    
+}
+
+/**
+ * Fixes the tree if it is breaking RBT rules.
+ *
+ * @param t - type tree, the tree.
+ *
+ * @return returns the fixed tree.
+ */
+static tree tree_fix(tree t){
+    if (IS_RED(t->left) && IS_RED(t->left->left)){
+        if (IS_RED(t->right)){
+            t->colour = RED;
+            t->left->colour = BLACK;
+            t->right->colour = BLACK;
+        } else {
+            t = right_rotate(t);
+            t->colour = BLACK;
+            t->right->colour = RED;
+        }
+    } else if (IS_RED(t->left) && IS_RED(t->left->right)){
+        if (IS_RED(t->right)){
+            t->colour = RED;
+            t->left->colour = BLACK;
+            t->right->colour = BLACK;
+        } else {
+            t->left = left_rotate(t->left);
+            t = right_rotate(t);
+            t->colour = BLACK;
+            t->right->colour = RED;
+        }
+    } else if (IS_RED(t->right) && IS_RED(t->right->left)){
+        if (IS_RED(t->left)){
+            t->colour = RED;
+            t->left->colour = BLACK;
+            t->right->colour = BLACK;
+        } else {
+            t->right = right_rotate(t->right);
+            t = left_rotate(t);
+            t->colour = BLACK;
+            t->left->colour = RED;
+        }
+    } else if (IS_RED(t->right) && IS_RED(t->right->right)){
+        if (IS_RED(t->left)){
+            t->colour = RED;
+            t->left->colour = BLACK;
+            t->right->colour = BLACK;
+        } else {
+            t = left_rotate(t);
+            t->colour = BLACK;
+            t->left->colour = RED;
+        }
+    }
+    if (t == root_node){
+        t->colour = BLACK;
+    }
+    return t;
+}
+
+/**
+ * Inserts string into the tree.
+ *
+ * @param t - type tree, the tree.
+ * @param str - type char*, the string to be inserted into the tree.
+ *
+ * @return returns the string.
+ */
+tree tree_insert(tree t, char *str){
+    
+    if (t == NULL){
+        t = emalloc(sizeof *t);
+        t->key = emalloc((strlen(str) + 1) * sizeof *str);
+        t->left = tree_new(tree_type);
+        t->right = tree_new(tree_type);
+        /* MUST BE BLACK IF ROOT NODE FIX!!! */
+        if (tree_type == RBT){
+            t->colour = RED;
+        }
+        strcpy(t->key, str);
+        t->frequency = 1;
+        if (no_insertion_calls == 0){
+            root_node = t;
+            no_insertion_calls++;
+        }
+        return t;
+    } else {
+        if (strcmp(t->key, str) == 0){ /* the same */
+            t->frequency++;
+            return t;
+        } else {
+            if (strcmp(t->key, str) > 0){ /* key to be inserted is smaller */
+                t->left = tree_insert(t->left, str);
+                if (tree_type == RBT){
+                    t = tree_fix(t);
+                }
+                return t;
+            } else {
+                t->right = tree_insert(t->right, str);
+                if (tree_type == RBT){
+                    t = tree_fix(t);
+                }
+                return t;
+            }
+        }
+    }
+}
+
+void tree_preorder(tree t, void f(int freq, char *str)){
+    if (t == NULL){
+        return;
+    } else {
+        /* NEED TO MAKE IT SO THAT ROOT NODE IS ALWAYS BLACK AFTER FIX UP
+          if (tree_type == RBT){
+            if (IS_RED(t)){
+                printf("red: ");
+            } else {
+                printf("black: ");
+            }
+            }*/
+        f(t->frequency, t->key);
+        if (t->left != NULL){
+            tree_preorder(t->left, f);
+        }
+        if (t->right != NULL){
+            tree_preorder(t->right, f);
+        }
+    }
+}
+
+/**
+ * Find the depth of the given tree.
+ *
+ * @param t - type tree, the tree.
+ *
+ * @return int - return the depth of the tree.
+ */
+int tree_depth(tree t){
+    int max_count = 0;
+    int left_depth = -1;
+    int right_depth = -1;
+    if (t == NULL){
+        return -1;
+    } else {
+        if (t->left == NULL && t->right == NULL){
+            return 0;
+        } else {
+            left_depth = tree_depth(t->left);
+            right_depth = tree_depth(t->right);
+            if (left_depth > right_depth){
+                return max_count = left_depth + 1; 
+            } else {
+                return max_count = right_depth + 1;
+            }
+        }
+    }
+}
+
+
+
